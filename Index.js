1c232c31ab156588dbe44f38528dcf704aa9ebc5
Manav Tanwar <84010785+ManavTanwar@users.noreply.github.com>
2021-07-22 11:56:10 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,27 @@
+// document.querySelector("#cont").addEventListener("click",function(){
+//     window.scrollTo({
+//         top:4000,
+//         left:0,
+//         behavior:"smooth"
+//     });
+// } );
+
+// document.querySelector("#skill").addEventListener("click",function(){
+//     window.scrollTo({
+//         top:750,
+//         left:0,
+//         behavior:"smooth"
+//     });
+// });
+
+// function showhide(){
+//     $('#skillsContainer').slideToggle(200);
+// }
+
+document.querySelector('.langbtn').addEventListener('click', function() {
+    document.querySelector('.Lang-column').classList.toggle('expand');
+  });
+
+
+
+
